778a1390d279f4ad23afc6512817b7617f14a048
Kiran PV <kiran@gajadigital.com>
2016-01-05 11:33:58 +0530

    Committing the project structure an initial settings

---
+++ app/src/main/java/com/kiranpv/sampletab/custom/CustomViewPager.java
@@ -0,0 +1,15 @@
+package com.kiranpv.sampletab.custom;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+
+/**
+ * Created by kiranpv on 05/01/16.
+ */
+public class CustomViewPager extends ViewPager {
+
+    public CustomViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
