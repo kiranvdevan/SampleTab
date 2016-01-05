be102014465f7ff5e9131c9249a502e4e2410f8e
Kiran PV <kiran@gajadigital.com>
2016-01-05 13:45:01 +0530

    Committing the changes to project structure

--- app/src/main/java/com/kiranpv/sampletab/activity/MainActivity.java
+++ app/src/main/java/com/kiranpv/sampletab/activity/MainActivity.java
@@ -1,33 +1,28 @@
 package com.kiranpv.sampletab.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.kiranpv.sampletab.R;
+import com.kiranpv.sampletab.custom.CustomViewPager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private CustomViewPager customViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
--- app/src/main/java/com/kiranpv/sampletab/custom/CustomViewPager.java
+++ app/src/main/java/com/kiranpv/sampletab/custom/CustomViewPager.java
@@ -3,13 +3,49 @@ package com.kiranpv.sampletab.custom;
 import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 
 /**
  * Created by kiranpv on 05/01/16.
+ * Description:
+ *  # Making a CustomViewPager so as to modify the swipe feature
+ *      (paging) of the view pager according to the requirements.
  */
 public class CustomViewPager extends ViewPager {
 
+    private boolean enabled;
+
     public CustomViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.enabled  = true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return enabled ?  super.onTouchEvent(event) : false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        return enabled ? super.onInterceptTouchEvent(event) : false;
+    }
+
+    /*
+    // To permanently disable the scroll of view-pager.
+        @Override
+    protected boolean canScroll(View v, boolean checkV, int dx, int x, int y) {
+        if(v instanceof MapView){
+            return true;
+        }
+        return super.canScroll(v, checkV, dx, x, y);
+    }
+     */
+
+    public void setPagingEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public boolean isPagingEnabled() {
+        return enabled;
     }
 }
